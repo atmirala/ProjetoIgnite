7a79b8ccfb09d22815fbf9b815aaca74713dba89
atmirala <atmirala@gmail.com>
2021-04-09 08:48:16 -0300

    fix stream

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,7 +1,19 @@
+/* eslint-disable import/no-unresolved */
+import csvParse from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUseCase {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   execute(file: Express.Multer.File): void {
-    console.log(file);
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
