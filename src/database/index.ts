4b6ade4c9dfe20bdd1c5c38883e7d3d9646f2a5b
atmirala <atmirala@gmail.com>
2021-04-16 10:41:04 -0300

    fix bugs

---
+++ src/database/index.ts
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-unresolved */
+import { createConnection, getConnectionOptions } from "typeorm";
+
+interface IOptions {
+  host: string;
+}
+
+getConnectionOptions().then((options) => {
+  const newOptions = options as IOptions;
+  newOptions.host = "database";
+  createConnection({
+    ...options,
+  });
+});
--- src/databese/index.ts
+++
@@ -1,4 +0,0 @@
-/* eslint-disable import/no-unresolved */
-import { createConnection } from "typeorm";
-
-createConnection();
