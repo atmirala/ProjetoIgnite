4f01e61afb63a18a9ff10ff8ac8e4015956c5aea
atmirala <atmirala@gmail.com>
2021-04-16 09:16:42 -0300

    fix some issues on docker-compose.yml

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import swaggerUi from "swagger-ui-express";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
-import "./database";
+import "./database/index.ts";
 
 // eslint-disable-next-line import/no-unresolved
 
@@ -19,4 +19,4 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(3000, () => console.log("Server is running!"));
